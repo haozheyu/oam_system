53fe0351ebd16b20be19037f3ae336ccdf0c1cfb
haozheyu <haozheyu@prevailcloud.com>
2022-07-06 22:48:52 +0800

    arco-cli: initialize project

--- admin-ui/src/router/routes/modules/dashboard.ts
+++ admin-ui/src/router/routes/modules/dashboard.ts
@@ -22,18 +22,6 @@ const DASHBOARD: AppRouteRecordRaw = {
         roles: ['*'],
       },
     },
-    /** simple */
-    {
-      path: 'monitor',
-      name: 'Monitor',
-      component: () => import('@/views/dashboard/monitor/index.vue'),
-      meta: {
-        locale: 'menu.dashboard.monitor',
-        requiresAuth: true,
-        roles: ['admin'],
-      },
-    },
-    /** simple end */
   ],
 };
 
