5b4c3e65a203e9455d26535c9aaf5783bc85e36b
1216108213@qq.com <1216108213@qq.com>
2022-07-18 15:47:23 +0800

    update .gitignore

---
+++ taro-ui/src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <Counter />
+    123123
+  </view>
+</template>
+
+<script>
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Counter
+  }
+}
+</script>
